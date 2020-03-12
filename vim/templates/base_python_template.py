17410e540d001080fc36449d43968f9bb7e6d417
Jitendra Kumar <jitendra.iitp@gmail.com>
2020-03-12 09:12:24 +0530

    python template on vim

---
+++ vim/templates/base_python_template.py
@@ -0,0 +1,12 @@
+
+
+def __doAllTestsPass():
+    allTestsPassing = True
+    # Start writing here.
+    return allTestsPassing
+
+if __name__ == '__main__':
+    if __doAllTestsPass():
+        print('All Tests are Passing.')
+    else:
+        print('Failed for one or more tests.')
