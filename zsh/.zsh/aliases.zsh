31c6787a88e2c78ca2e1c378b9fe86bbf5ad6d4f
Jitendra Kumar <jitendra.iitp@gmail.com>
2021-02-13 14:57:33 +0530

    Adding zsh configs

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,26 @@
+
+# Custom aliases
+alias ll="ls -al"
+alias l="ls -a"
+alias cls="clear"
+alias q="exit"
+alias sz="source ~/.zshrc"
+alias g++="g++ -std=c++14"
+
+#Neovim
+alias v="nvim"
+alias vi="nvim"
+
+
+# zsh
+alias zconf="vim ~/.zshrc"
+alias vconf="vim ~/.vimrc"
+
+# Youtube-dl
+alias youtube-dl-playlist="youtube-dl -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'"
+
+# Git
+alias gst="git status"
+alias gm="git commit -m"
+alias gma="git commit -am"
+alias gpl="git log --graph --pretty=format:'%Cred%h%Creset %C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --"
